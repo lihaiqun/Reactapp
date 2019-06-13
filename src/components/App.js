ad2f89a9d136a247781bb049aafd8cf97e41ada4
lihaiqun <1289181815@qq.com>
2019-06-13 16:40:00 +0800

    reset status way 3 in render hook function with arrow

--- src/components/App.js
+++ src/components/App.js
@@ -2,21 +2,20 @@ import React, { Component } from 'react';
 class Com extends Component {
   constructor(props) {
     super(props);
-    this.changeFn = this.changeFn.bind(this)
     this.state = {
       msg: 'Can I change'
     }
   }
   changeFn () {
     this.setState({
-      msg: 'no you can not do like that'
+      msg: 'It is none of my business'
     })
   }
   render () {
     return (
       <div>
         { this.state.msg }
-        <button onClick={ this.changeFn }>click me</button>
+        <button onClick={ () => this.changeFn() }>click me</button>
       </div>
     )
   }
